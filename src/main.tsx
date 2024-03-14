2ed5ae432cfa606cd6643a17b15f6f68779b0d1d
HenrickyL <HenrickyL1@gmail.com>
2024-03-13 22:37:06 -0300

    test toggle Theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,39 @@ import ReactDOM from 'react-dom/client'
 import { Providers } from './_hooks/index.tsx'
 import { RouterProvider } from 'react-router-dom'
 import {routes} from './_routes'
+import styled from 'styled-components'
+import { useTheme } from './_hooks/theme.tsx'
+
+
+const STY = styled.div`
+  position: absolute;
+  display: flex;
+  top: 0;
+  right:0;
+
+  button{
+    width: 2rem;
+    height: 2rem;
+    padding: 8px;
+    border-radius: 50%;
+    background-color: blue;
+  }
+`
+const TestTheme = ()=>{
+  const  {toggle} = useTheme()
+  return(
+    <STY>
+      <button onClick={toggle}>T</button>
+    </STY>
+  )
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Providers>
       <RouterProvider router={routes} />
+      <TestTheme />
     </Providers>
   </React.StrictMode>,
 )
